13af617116f2cd4b292179296074017ebb1f1979
wangguoxi <wangguoxi@darryring.com>
2026-07-05 15:58:55 +0800

    test:mcp server db_exe tool test

---
+++ main.py
@@ -0,0 +1,9 @@
+from datasource_mcp import db_exe
+
+def main():
+    sql = "select * from chat_message;"
+    result = db_exe(sql)
+    print(f'result is {result}')
+
+if __name__ == "__main__":
+    main()
